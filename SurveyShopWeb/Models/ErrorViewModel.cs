6795056135e1d542ee5688f603a8e35d01780838
SingleUnitBox <cezaryzukowski90@gmail.com>
2023-09-10 11:20:53 +0100

    1st commit

---
+++ SurveyShopWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SurveyShopWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
