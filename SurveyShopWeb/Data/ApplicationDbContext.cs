b1d71f21a7e50278a4502d53e5577880cfd6ba49
SingleUnitBox <cezaryzukowski90@gmail.com>
2023-09-10 13:40:50 +0100

    add Category Index Page

---
+++ SurveyShopWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyShopWeb.Models;
+
+namespace SurveyShopWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var categories = new List<Category>
+            {
+                new Category { Id = 1, Name = "Total Station", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "GNSS", DisplayOrder= 2 },
+                new Category { Id = 3, Name = "Controller", DisplayOrder = 3 }   
+            };
+            modelBuilder.Entity<Category>().HasData(categories);
+        }
+    }
+}
