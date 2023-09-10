5f113c2a952518eca1ab1abb64fa2414a6e9679b
SingleUnitBox <cezaryzukowski90@gmail.com>
2023-09-10 21:52:16 +0100

    add Edit, Delete Category

--- SurveyShopWeb/Controllers/CategoryController.cs
+++ SurveyShopWeb/Controllers/CategoryController.cs
@@ -28,9 +28,66 @@ namespace SurveyShopWeb.Controllers
             {
                 _applicationDbContext.Categories.Add(category);
                 _applicationDbContext.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction(nameof(Index));
             }
             return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category category = _applicationDbContext.Categories.FirstOrDefault(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                var categoryFromDb = _applicationDbContext.Categories.FirstOrDefault(x => x.Id == category.Id);
+                categoryFromDb.Name = category.Name;
+                categoryFromDb.DisplayOrder = category.DisplayOrder;
+                _applicationDbContext.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category category = _applicationDbContext.Categories.FirstOrDefault(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            var categoryFromDb = _applicationDbContext.Categories.FirstOrDefault(x => x.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            _applicationDbContext.Categories.Remove(categoryFromDb);
+            _applicationDbContext.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction(nameof(Index));
         }
     }
 }
